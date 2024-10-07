293a153168978729777a8d33bb2d6887c2e5c3cd
KAlexdark <152426934+KAlexdark@users.noreply.github.com>
2024-10-08 00:57:50 +0300

    Update DZ#1.cpp

--- DZ#1.cpp
+++ DZ#1.cpp
@@ -1,14 +1,27 @@
 #inclube <iostream>
 
-int schet(int x){
-  x = x + x;
-  std::cout << x;
+int schet(int x) {
+  int schetchic{ 0 };
+  int peremens_vivod{ 0 };
+  while (schetchic += 2) {
+    peremens_vivod += % 10;
+    x + x /10;
+    schetchic++;
+  }
+  std::cout << peremens_vivod << "\n";
   return 0;
 }
 
-int proverka(int proverka_peremens) {
+int schet2(int x) {
+  int y = ((x % 100) / 10) * ((x % 100) / 10);
+  int t = (x / 100) + (x % 100);
+  t > y ? std::cout << "Yes" : std::cout << "No";
+  return 0;
+}
+
+int proverka(int proverka_peremens, int x) {
   if(99 < proverka_peremens && proverka_peremens < 1000){
-    schet
+    x == 1 ? schet(proverka_peremens): schet2(proverka_peremens);
   }
   else
   {
@@ -16,3 +29,31 @@ int proverka(int proverka_peremens) {
     return 0;
   }
 }
+
+int vivod(int proverka_vhod,int peremen_vibor) {
+  std::cout << "Program vibor cactvia\n";
+  std::cout << "1: Zadacha_#1\n";
+  std::cout << "2: Zadacha_#2\n";
+  std::cin >> peremen_vibor;
+  switch(peremen_vibor)
+  {
+  case 1:
+      std::cout << "Vedite chislo: \n";
+      std::cin >> peremen_vibor;
+      proverka(proverka_vhod, peremen_vibor);
+      break;
+  case 2:
+      std::cout << "Vedite chislo: \n";
+      std::cin >> peremen_vibor;
+      proverka(proverka_vhod, peremen_vibor);
+      break;
+  default:
+      std::cout << "Error2: нет такого номера\n";
+      break;
+  }
+  return 0;
+}
+
+ int main (int vibor) {
+   vibod();
+ }
